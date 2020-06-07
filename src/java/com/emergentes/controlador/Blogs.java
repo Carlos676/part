984851c0fcb8cf98b488c78affdb3a3ab2a4bd87
carlos mamani <juancarlosmamanimamani011@gmail.com>
2020-06-07 15:42:16 -0400

    Practica 4 login y BLogs

--- src/java/com/emergentes/controlador/Blogs.java
+++ src/java/com/emergentes/controlador/Blogs.java
@@ -165,7 +165,6 @@ public class Blogs extends HttpServlet {
      *
      * @return a String containing servlet description
      */
-    @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
